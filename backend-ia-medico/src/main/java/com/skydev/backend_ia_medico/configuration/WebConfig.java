1c5657f7f229605aa1dc3155db79ba4741a091ce
SkyDev24 <anyelonuob2004@gmail.com>
2025-11-30 21:10:55 -0500

    Cors

--- backend-ia-medico/src/main/java/com/skydev/backend_ia_medico/configuration/WebConfig.java
+++ backend-ia-medico/src/main/java/com/skydev/backend_ia_medico/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // aplica a todos los endpoints
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
